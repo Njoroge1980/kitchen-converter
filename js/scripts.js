e2971db1051638baf95828528d3379d038af6dfb
Njoroge Mungai <njorogemungai1980@gmail.com>
2020-04-15 00:35:13 +0300

    add the initial folder structure

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var gallons = function (liters) {
+    return liters*0.219969
+    }
+    var liters = parseInt(prompt("Enter amount in liters"));
+    var result =gallons(liters);
+    alert("the amount in gallons is " + result);
+    undefined
+    var pounds = function(kgs) {
+    return kgs*2.205
+    }
+    var kgs = parseInt(prompt("Enter amount in kgs"));
+    var result = pounds(kgs);
+    alert("the amount in pounds is " + result);
